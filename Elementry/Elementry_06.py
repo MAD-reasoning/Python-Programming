c662ea90c7c2ee3baba50803eafcd650cc8e6f57
MAD-reasoning <43748834+MAD-reasoning@users.noreply.github.com>
2018-10-28 12:38:25 +0530

    Computing sum or product
    Asks the user for a number n and gives them the possibility to choose between computing the sum and computing the product of 1…n.

--- Elementry/Elementry_06.py
+++ Elementry/Elementry_06.py
@@ -1,5 +1,5 @@
 """ Write a program that asks the user for a number n and gives them the possibility to choose between computing the sum
- and computing the product of 1,…,n.
+ and computing the product of 1 to n.
 """
 
 num_sum = 0
