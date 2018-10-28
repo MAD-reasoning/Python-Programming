9f9d4abc6e2494da723829221ddfdc48bd5d124a
MAD-reasoning <43748834+MAD-reasoning@users.noreply.github.com>
2018-10-28 12:40:17 +0530

    Table upto 12
    Multiplication table for numbers up to 12.

--- Elementry/Elementry_07.py
+++ Elementry/Elementry_07.py
@@ -1,4 +1,4 @@
-# Write a program that prints a multiplication table for numbers up to 12.
+# Write a program that prints a multiplication table for numbers upto 12.
 
 try:
     number = int(input("Enter a natural number(max 12): "))
