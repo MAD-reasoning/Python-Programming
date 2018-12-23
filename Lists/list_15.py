ac10f0ccf0fde6f9f6f65b1068e516549523c9d1
Shivam Singh Baiswar <43748834+MAD-reasoning@users.noreply.github.com>
2018-12-23 08:32:13 +0530

    Delete list_15.py

--- Lists/list_15.py
+++
@@ -1,6 +0,0 @@
-""" Write functions that add, subtract, and multiply two numbers in their digit-list representation (and return a new
-digit list). If you’re ambitious you can implement Karatsuba multiplication. Try different bases. What is the best base
-if you care about speed? If you couldn’t completely solve the prime number exercise above due to the lack of large
-numbers in your language, you can now use your own library for this task."""
-
-
